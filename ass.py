cafa1149acfbf9dc138d0046cecf55e14f37e125
Dingjiangjiang <35858485+Dingjiangjiang@users.noreply.github.com>
2022-02-24 21:21:18 +0800

    Add files via upload

---
+++ ass.py
@@ -0,0 +1,25 @@
+import json
+import os
+import os.path as osp
+from collections import OrderedDict
+
+import numpy as np
+
+from mmpose.core.evaluation.top_down_eval import (keypoint_nme,
+                                                  keypoint_pck_accuracy)
+from mmpose.datasets.builder import DATASETS
+from mmpose.datasets.datasets.animal import AnimalAP10KDataset
+
+
+@DATASETS.register_module()
+class AnimalCatsDataset(AnimalAP10KDataset):
+
+	def __init__(self,
+				 ann_file,
+				 img_prefix,
+				 data_cfg,
+				 pipeline,
+				 dataset_info=None,
+				 test_mode=False):
+		super().__init__(
+			ann_file, img_prefix, data_cfg, pipeline, dataset_info, test_mode=test_mode)
